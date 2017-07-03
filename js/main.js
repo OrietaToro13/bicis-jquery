f057721206328c6d7e9256cd9728d873ea7f30ee
Orieta Toro <orieta.toro@gmail.com>
2017-07-02 23:21:40 -0400

    validacion jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-function validateForm(){
+function validateForm(v){
 	/* Escribe tú código aquí */
-}
+	var letras= ("a", "b", "c", "d","e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+	//verificar que no este vacio el campo y que sea solo letras//
+	$(document).ready(function(){
+		var name = $("#name").val();
+		var name = $("#lastname").val();
+		var name = $("#input-email").val();
+		var name = $("#input-password").val();
+
+		$('#name').click(function()
+	if($('#name').val().trim() === ''){
+		alert("Ingrese su Nombre");
+	}
+	else{#name
+
+	}	
+	})
+	
+};
+
